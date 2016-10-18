d0d91ecf291f9f052fae2658d5b17fb21dcad523
Skage Spjelkavik <skage.spjelkavik@okb.no>
2016-10-18 16:29:12 +0200

    Step 8 done: Hooked up Redux, and displayed default data.

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import reset from './reset.css';
 import app from './app.css';
-import TopList from './top-list';
+import CurrentTopList from './current-top-list.container';
 import Header from './header';
-
-const topList = [
-  { id: 1, name: 'Håkon', weeklyCount: 5, totalCount: 25 },
-  { id: 2, name: 'Asbjørn', weeklyCount: 2, totalCount: 18 },
-  { id: 3, name: 'Tore', weeklyCount: 11, totalCount: 17 },
-  { id: 4, name: 'Skage', weeklyCount: 7, totalCount: 23 },
-  { id: 5, name: 'Espen', weeklyCount: 6, totalCount: 42 },
-  { id: 6, name: 'Boye', weeklyCount: 4, totalCount: 41 }
-].sort((a, b) => b.weeklyCount - a.weeklyCount);
 
 const App = () => {
   return (
     <div className={app.container}>
       <Header title="17.10 - 21.10"/>
-      <TopList list={topList}/>
+      <CurrentTopList />
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,19 @@ import { createStore } from 'redux';
 import reducers from './redux';
 import App from './app';
 
+const topList = [
+  { id: 1, name: 'Håkon', weeklyCount: 5, totalCount: 25 },
+  { id: 2, name: 'Asbjørn', weeklyCount: 2, totalCount: 18 },
+  { id: 3, name: 'Tore', weeklyCount: 11, totalCount: 17 },
+  { id: 4, name: 'Skage', weeklyCount: 7, totalCount: 23 },
+  { id: 5, name: 'Espen', weeklyCount: 6, totalCount: 42 },
+  { id: 6, name: 'Boye', weeklyCount: 4, totalCount: 41 }
+].sort((a, b) => b.weeklyCount - a.weeklyCount);
+
 const rootElement = document.getElementById('app');
-const store = createStore(reducers);
+const store = createStore(reducers, {
+  topList: topList
+});
 
 ReactDOM.render(
   <AppContainer>
