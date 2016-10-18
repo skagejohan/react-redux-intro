7073c2543be5a4f45efaf6a285c5f996b0f97576
Skage Spjelkavik <skage.spjelkavik@okb.no>
2016-10-18 10:20:06 +0200

    Step 3 done. Added loader and example code.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('Hello world');
+const messageParts = ['Hello', 'world'];
+const message = messageParts.reduce((prev, curr) => `${prev} ${curr}`, '');
+
+console.log(message);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
     path: '__dirname' +  '/dist',
     filename: 'bundle.js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
